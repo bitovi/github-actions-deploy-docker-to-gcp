f08b043bfab6e833a439e04e45e291feb89041da
Max Cascone <max@bitovi.com>
2023-02-01 15:46:47 -0600

    incorporate google naming rules

--- operations/_scripts/deploy/check_bucket_name.sh
+++ operations/_scripts/deploy/check_bucket_name.sh
@@ -2,11 +2,13 @@
 
 set -e 
 
-### S3 Buckets name must follow AWS rules. Info below.
-### https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+### Google Cloud Storage Buckets name must follow GCP rules. Info below.
+### https://cloud.google.com/storage/docs/buckets#naming
 
 function checkBucket() {
   # check length of bucket name
+  # TODO: rule is actually: Bucket names must contain 3-63 characters. 
+  # Names containing dots can contain up to 222 characters, but each dot-separated component can be no longer than 63 characters.
   if [[ ${#1} -lt 3 || ${#1} -gt 63 ]]; then
     echo "::error::Bucket name must be between 3 and 63 characters long."
     exit 1
@@ -46,11 +48,26 @@ function checkBucket() {
     exit 1
   fi
   
-  # check that bucket name does not end with the suffix -s3alias
-  if [[ $1 =~ -s3alias$ ]]; then
-    echo "::error::Bucket name cannot end with the suffix -s3alias."
+  # # check that bucket name does not end with the suffix -s3alias
+  # if [[ $1 =~ -s3alias$ ]]; then
+  #   echo "::error::Bucket name cannot end with the suffix -s3alias."
+  #   exit 1
+  # fi
+
+  # check that bucket name does not start with the prefix 'goog'
+  if [[ $1 =~ ^goog ]]; then
+    echo "::error::Bucket name cannot start with the prefix 'goog'."
     exit 1
   fi
+
+  # check that bucket name does not contain "google" or close misspellings, such as "g00gle".
+
+  if [[ $1 =~ google|g00gle|go0gle|g0ogle|60061e|6oo6l3|600g13 ]]; then
+    echo "::error::Bucket name cannot contain 'google' or close misspellings, such as 'g00gle'."
+    exit 1
+  fi
+
+
 }
 
 checkBucket $1
